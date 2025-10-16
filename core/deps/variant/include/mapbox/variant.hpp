e97ee58532401e4c418aead5c857e515c280506f
farfromrefug <dev@akylas.fr>
2025-10-16 16:31:10 +0200

    chore: update to c++20

--- core/deps/variant/include/mapbox/variant.hpp
+++ core/deps/variant/include/mapbox/variant.hpp
@@ -176,7 +176,7 @@ struct enable_if_type
 template <typename F, typename V, typename Enable = void>
 struct result_of_unary_visit
 {
-    using type = typename std::result_of<F(V&)>::type;
+    using type = typename std::invoke_result<F, V&>::type;
 };
 
 template <typename F, typename V>
@@ -188,7 +188,7 @@ struct result_of_unary_visit<F, V, typename enable_if_type<typename F::result_ty
 template <typename F, typename V, typename Enable = void>
 struct result_of_binary_visit
 {
-    using type = typename std::result_of<F(V&, V&)>::type;
+    using type = typename std::invoke_result<F, V&, V&>::type;
 };
 
 template <typename F, typename V>
